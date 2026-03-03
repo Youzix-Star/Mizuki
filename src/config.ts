f3f5ff03250860646d265611ace4ab777f0d6f72
Youzix_Star <166191037+Youzix-Star@users.noreply.github.com>
2026-03-03 08:27:17 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -220,9 +220,9 @@ export const siteConfig: SiteConfig = {
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "游趣体",
+			fontFamily: "萝莉体 第二版",
 			fontWeight: "500",
-			localFonts: ["ChildFunSans.ttf"],
+			localFonts: ["萝莉体 第二版.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
