6ee0551ec55f59711c5025ce7a7ff9745207b3af
Youzix_Star <166191037+Youzix-Star@users.noreply.github.com>
2026-04-26 00:48:29 +0800

    Update Twikoo envId URL in commentConfig

--- src/config.ts
+++ src/config.ts
@@ -450,7 +450,7 @@ export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
-		envId: "https://youzix.netlify.app/.netlify/functions/twikoo",
+		envId: "http://youzix.dpdns.org/.netlify/functions/twikoo",
 		lang: SITE_LANG,
 	},
 	giscus: {
