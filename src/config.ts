256d8cc4f4afda7c46a5b12ede44bfb47332ef4d
Youzix_Star <166191037+Youzix-Star@users.noreply.github.com>
2026-04-26 11:38:40 +0800

    Update profile configuration with new user details

--- src/config.ts
+++ src/config.ts
@@ -369,7 +369,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "まつざか ゆき",
+	name: "不会取名的屑星",
 	bio: "世界は大きい、君は行かなければならない",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
@@ -379,27 +379,27 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Bilibili",
 			icon: "fa7-brands:bilibili",
-			url: "https://space.bilibili.com/701864046",
+			url: "https://space.bilibili.com/1015832090",
 		},
 		{
 			name: "Gitee",
 			icon: "mdi:git",
-			url: "https://gitee.com/matsuzakayuki",
+			url: "https://gitee.com/Youzix_Star",
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/matsuzaka-yuki",
+			url: "https://github.com/Youzix-Star",
 		},
 		{
-			name: "Codeberg",
-			icon: "simple-icons:codeberg",
-			url: "https://codeberg.org",
+			name: "Email",
+			icon: "material-symbols:alternate_email",
+			url: "mailto:wxd1y12r@gmail.com",
 		},
 		{
-			name: "Discord",
-			icon: "fa7-brands:discord",
-			url: "https://discord.gg/MqW6TcQtVM",
+			name: "QQ",
+			icon: "simple-icons:qq",
+			url: "https://qm.qq.com/q/K9fSWDAskA",
 		},
 	],
 };
